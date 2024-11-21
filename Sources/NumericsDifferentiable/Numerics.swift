9c40054475bb3d4b3bdfeaf0a07c3c2cb1d553c5
Jaap Wijnen <jaap@passivelogic.com>
2024-11-21 10:41:48 +0100

    feat: create initial package

---
+++ Sources/NumericsDifferentiable/Numerics.swift
@@ -0,0 +1,6 @@
+// A module that re-exports Swift Numerics API and the Differentiable extensions to the Swift Numerics API
+@_exported import Numerics
+// Swift Numerics modules with Differentiable extensions
+@_exported import RealModuleDifferentiable
+// No differentiable implementations for the `ComplexModule` yet
+//@_exported import ComplexModuleDifferentiable
---
+++ Sources/RealModuleDifferentiable/RealModule.swift
@@ -0,0 +1,2 @@
+// Export the RealModule API from Swift Numerics which this module extends with `Differentiable` support
+@_exported import RealModule
