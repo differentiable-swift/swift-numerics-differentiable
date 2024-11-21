5ce9eb91a5303bf393943f4cf6b2c7a82b535ae8
Jaap Wijnen <jaap@passivelogic.com>
2024-11-21 10:41:48 +0100

    feat: create initial package

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-numerics-differentiable",
+    products: [
+        .library(
+            name: "NumericsDifferentiable",
+            targets: ["NumericsDifferentiable"]
+        ),
+        .library(
+            name: "RealModuleDifferentiable",
+            targets: ["RealModuleDifferentiable"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.2"),
+    ],
+    targets: [
+        .target(
+            name: "NumericsDifferentiable",
+            dependencies: [
+                .product(name: "Numerics", package: "swift-numerics"),
+                "RealModuleDifferentiable",
+            ]
+        ),
+        .target(
+            name: "RealModuleDifferentiable",
+            dependencies: [
+                .product(name: "RealModule", package: "swift-numerics"),
+            ]
+        ),
+        .testTarget(
+            name: "RealModuleDifferentiableTests",
+            dependencies: [
+                "RealModuleDifferentiable",
+            ]
+        ),
+    ]
+)
