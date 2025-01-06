505e740c167feac62f05bbf765374059391ecb31
Jaap Wijnen <jaap@passivelogic.com>
2025-01-06 13:05:59 +0100

    feat: add ComplexModule so we can extend swift-numerics's complex module with differentiable methods

---
+++ Sources/ComplexModuleDifferentiable/ComplexModule.swift
@@ -0,0 +1,2 @@
+// Export the ComplexModule API from Swift Numerics which this module extends with `Differentiable` support
+@_exported import ComplexModule
--- Sources/NumericsDifferentiable/Numerics.swift
+++ Sources/NumericsDifferentiable/Numerics.swift
@@ -1,6 +1,4 @@
-// A module that re-exports Swift Numerics API and the Differentiable extensions to the Swift Numerics API
+// This module re-exports the Swift Numerics API and the Differentiable extensions to the Swift Numerics API
+@_exported import ComplexModuleDifferentiable
 @_exported import Numerics
-// Swift Numerics modules with Differentiable extensions
 @_exported import RealModuleDifferentiable
-// No differentiable implementations for the `ComplexModule` yet
-// @_exported import ComplexModuleDifferentiable
